4f23ce7ac69f644e3ad8fbfd95824f4e064cf578
philip cleaver <socks4penguins@gmail.com>
2021-08-01 09:47:30 +0000

    block functions

--- src/features/blockly/WizardBlocks.js
+++ src/features/blockly/WizardBlocks.js
@@ -7,30 +7,44 @@ export default function WizardBlocks(props) {
   const { selectedBlock, workspace } = props;
   const [state, setState] = useState({});
 
-  function addBlock(parentBlock, inputName, newBlockType) {
-    var parentConnection = parentBlock.getInput(inputName).connection;
-    var childBlock = workspace.newBlock(newBlockType);
+  function makeBlock({ type, fields }) {
+    var childBlock = workspace.newBlock(type);
+    fields.forEach(field => {
+      try {
+        childBlock.setFieldValue(field.value, field.name);
+      } catch {
+        console.Console("couldn't set field", field);
+      }
+    });
     childBlock.initSvg();
     childBlock.render();
+  }
+  function addBlockToParent({ parentBlock, inputName, childBlock }) {
+    var parentConnection = parentBlock.getInput(inputName).connection;
     var childConnection = childBlock.previousConnection;
     parentConnection.connect(childConnection);
   }
 
   function getEmptyInputs(block) {
-    return block.inputList.filter(input => input.connection.targetConnection === null);
+    return block.inputList.filter(
+      input => input.connection && input.connection.targetConnection === null,
+    );
   }
 
   const wizardBlock =
-    selectedBlock && Object.keys(wizard_blocks).filter(type => selectedBlock.type)
-      ? Object.keys(wizard_blocks).filter(type => selectedBlock.type)[0]
+    selectedBlock && Object.keys(wizard_blocks).filter(type => selectedBlock.type === type)
+      ? wizard_blocks[Object.keys(wizard_blocks).filter(type => selectedBlock.type === type)[0]]
       : null;
-  console.log({ selectedBlock, empty: selectedBlock && getEmptyInputs(selectedBlock) });
+  // console.log({ selectedBlock, empty: selectedBlock && getEmptyInputs(selectedBlock) });
   return (
     <div className="blockly-wizard-blocks vertical layout">
       <Typography variant="body1">Wizard{selectedBlock && ' - ' + selectedBlock.type}</Typography>
       {wizardBlock && (
         <div className="horizontal layout justified">
           <TextField
+            onBlur={e => {
+              console.log('blur', wizardBlock, state.main, getEmptyInputs(selectedBlock));
+            }}
             onChange={e => setState({ ...state, main: e.target.value })}
             value={state.main}
           />
