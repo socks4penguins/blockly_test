a88964fe849507b25c2fa621b00d7a156919aa04
philip cleaver <socks4penguins@gmail.com>
2021-08-02 01:31:42 +0000

    ok

--- src/features/blockly/WizardBlocks.js
+++ src/features/blockly/WizardBlocks.js
@@ -10,46 +10,52 @@ export default function WizardBlocks(props) {
   const { selectedBlock, workspace } = props;
   const [state, setState] = useState({});
 
-  
-
-  console.log('first', selectedBlock && firstChildType(selectedBlock));
+  // console.log('first', selectedBlock && firstChildType(selectedBlock));
   const wizardBlock =
     selectedBlock &&
     wizard_blocks.filter(
       wiz =>
         selectedBlock.type === wiz.blockType &&
-        (!wiz.valueInputs[0].blockType || firstChildType(selectedBlock) === wiz.valueInputs[0].blockType),
+        (!wiz.valueInputs[0].blockType ||
+          firstChildType(selectedBlock) === wiz.valueInputs[0].blockType),
     ).length > 0
       ? wizard_blocks.filter(
           wiz =>
             selectedBlock.type === wiz.blockType &&
-            (!wiz.valueInputs[0].blockType || firstChildType(selectedBlock) === wiz.valueInputs[0].blockType),
+            (!wiz.valueInputs[0].blockType ||
+              firstChildType(selectedBlock) === wiz.valueInputs[0].blockType),
         )[0]
       : null;
   // console.log({ selectedBlock, wizardBlock });
+  function renderValueInput(valueInput) {
+    console.log({ valueInput });
+    return (
+      <div className="vertical layout">
+        
+        
+        <TextField
+          onBlur={e => {
+            const fields = valueInput.fields.map(field => {
+              return { field: field.field, value: state.main };
+            });
+            connectBlockToInput({
+              parentBlock: selectedBlock,
+              inputName: getEmptyInputs(workspace, selectedBlock, true)[0].name,
+              childBlock: makeBlock({ workspace, type: valueInput.blockType, fields }),
+            });
+          }}
+          onChange={e => setState({ ...state, main: e.target.value })}
+          value={state.main}
+        />
+      </div>
+    );
+  }
   return (
     <div className="blockly-wizard-blocks vertical layout">
       <Typography variant="body1">Wizard{wizardBlock && ' - ' + wizardBlock.prompt}</Typography>
-      {wizardBlock && Blockly.selected && (
+      {wizardBlock && (
         <div className="horizontal layout justified">
-          <TextField
-            onBlur={e => {
-              // console.log('blur', wizardBlock, state.main, getEmptyInputs(selectedBlock));
-              wizardBlock.valueInputs.forEach(newBlock => {
-                const fields = newBlock.fields.map(field => {
-                  return { field: field.field, value: state.main };
-                });
-                connectBlockToInput({
-                  parentBlock: selectedBlock,
-                  // inputName: 'ADD0',
-                  inputName: getEmptyInputs(workspace, selectedBlock, true)[0].name,
-                  childBlock: makeBlock({workspace, type: newBlock.blockType, fields }),
-                });
-              });
-            }}
-            onChange={e => setState({ ...state, main: e.target.value })}
-            value={state.main}
-          />
+          {wizardBlock.valueInputs.map(renderValueInput)}
           {wizardBlock.repeatChildren && <TextField />}
         </div>
       )}
