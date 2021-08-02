75f03f08988964464ff25f19ea7a3b170ca0350e
philip cleaver <socks4penguins@gmail.com>
2021-08-02 00:06:50 +0000

    blockly_helper

--- src/features/blockly/WizardBlocks.js
+++ src/features/blockly/WizardBlocks.js
@@ -2,70 +2,14 @@ import React, { useState } from 'react';
 import { Typography, TextField } from '@material-ui/core';
 import wizard_blocks from '../../data/wizard_blocks';
 import * as Blockly from 'blockly';
+import { connectBlockToInput, firstChildType, getEmptyInputs, makeBlock } from './blockly_helper';
 // import PropTypes from 'prop-types';
 
 export default function WizardBlocks(props) {
   const { selectedBlock, workspace } = props;
   const [state, setState] = useState({});
 
-  function makeBlock({ type, fields }) {
-    var childBlock = workspace.newBlock(type);
-    // debugger;
-    fields.forEach(field => {
-      try {
-        childBlock.setFieldValue(field.value, field.field);
-      } catch {
-        console.log("couldn't set field", field);
-      }
-    });
-    childBlock.initSvg();
-    childBlock.render();
-    return childBlock;
-  }
-
-  function firstChildType(block) {
-    return block.childBlocks_.length > 0 && block.childBlocks_[0].type;
-  }
-
-  function connectBlockToInput({ parentBlock, inputName, childBlock }) {
-    var parentConnection = parentBlock.getInput(inputName).connection;
-    var childConnection = childBlock.outputConnection;
-    // debugger;
-    const result = childConnection.connect(parentConnection);
-    console.log({ result });
-    parentBlock.initSvg();
-    parentBlock.render();
-
-    // var childBlock1 = workspace.newBlock('text');
-    // childBlock1.setFieldValue('Hello', 'TEXT');
-    // childBlock1.initSvg();
-    // childBlock1.render();
-
-    // var parentBlock1 = workspace.newBlock('text_print');
-    // parentBlock1.initSvg();
-    // parentBlock1.render();
-
-    // var parentConnection1 = parentBlock1.getInput('TEXT').connection;
-    // var childConnection1 = childBlock1.outputConnection;
-    // parentConnection1.connect(childConnection1);
-  }
-
-  function getEmptyInputs(block, shouldMake) {
-    const empty = block.inputList.filter(
-      input => input.connection && input.connection.targetConnection === null,
-    );
-
-    if (empty.length > 0) return empty;
-
-    if (shouldMake) {
-      // // add a new item
-      var new_input = block.appendInput_(1, 'ADD' + block.inputList.length);
-      block.itemCount_ = block.inputList.length;
-      return [new_input];
-    }
-
-    return [];
-  }
+  
 
   console.log('first', selectedBlock && firstChildType(selectedBlock));
   const wizardBlock =
@@ -97,8 +41,8 @@ export default function WizardBlocks(props) {
                 connectBlockToInput({
                   parentBlock: selectedBlock,
                   // inputName: 'ADD0',
-                  inputName: getEmptyInputs(selectedBlock, true)[0].name,
-                  childBlock: makeBlock({ type: newBlock.type, fields }),
+                  inputName: getEmptyInputs(workspace, selectedBlock, true)[0].name,
+                  childBlock: makeBlock({workspace, type: newBlock.type, fields }),
                 });
               });
             }}
---
+++ src/features/blockly/blockly_helper.js
@@ -0,0 +1,58 @@
+export function makeBlock({workspace, type, fields }) {
+  var childBlock = workspace.newBlock(type);
+  // debugger;
+  fields.forEach(field => {
+    try {
+      childBlock.setFieldValue(field.value, field.field);
+    } catch {
+      console.log("couldn't set field", field);
+    }
+  });
+  childBlock.initSvg();
+  childBlock.render();
+  return childBlock;
+}
+
+export function firstChildType(block) {
+  return block.childBlocks_.length > 0 && block.childBlocks_[0].type;
+}
+
+export function connectBlockToInput({ parentBlock, inputName, childBlock }) {
+  var parentConnection = parentBlock.getInput(inputName).connection;
+  var childConnection = childBlock.outputConnection;
+  // debugger;
+  const result = childConnection.connect(parentConnection);
+  console.log({ result });
+  parentBlock.initSvg();
+  parentBlock.render();
+
+  // var childBlock1 = workspace.newBlock('text');
+  // childBlock1.setFieldValue('Hello', 'TEXT');
+  // childBlock1.initSvg();
+  // childBlock1.render();
+
+  // var parentBlock1 = workspace.newBlock('text_print');
+  // parentBlock1.initSvg();
+  // parentBlock1.render();
+
+  // var parentConnection1 = parentBlock1.getInput('TEXT').connection;
+  // var childConnection1 = childBlock1.outputConnection;
+  // parentConnection1.connect(childConnection1);
+}
+
+export function getEmptyInputs(workspace, block, shouldMake) {
+  const empty = block.inputList.filter(
+    input => input.connection && input.connection.targetConnection === null,
+  );
+
+  if (empty.length > 0) return empty;
+
+  if (shouldMake) {
+    // // add a new item
+    var new_input = block.appendInput_(1, 'ADD' + block.inputList.length);
+    block.itemCount_ = block.inputList.length;
+    return [new_input];
+  }
+
+  return [];
+}
